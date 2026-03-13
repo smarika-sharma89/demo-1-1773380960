967a8a0029074943108f9803efacfce9ee268ce1
smarika-sharma89 <smarika.sharma@varicon.com.au>
2026-03-13 11:34:32 +0545

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+      },
+    },
+  },
+  plugins: [],
+}
